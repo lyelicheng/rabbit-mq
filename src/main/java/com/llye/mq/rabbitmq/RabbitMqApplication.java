7639063b0150b06e4d080e519cbe68dc3ebf4e4e
joycelye <joycelye1991@gmail.com>
2023-09-09 21:20:38 +0800

    init

---
+++ src/main/java/com/llye/mq/rabbitmq/RabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.llye.mq.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqApplication.class, args);
+	}
+
+}
